23dc0c13978c4d6cca80e946ae0f4721f5a0a768
LuticaCANARD <presan100@gmail.com>
2023-04-27 15:01:48 +0900

    k8s

--- src/server.ts
+++ src/server.ts
@@ -9,3 +9,5 @@ import { WebSocket } from "ws";
 // "실행은 도커로 한다. 관리가 편해지기 떄문!!!!!"
 // Do : check about discord server. We need Kafka?  or redis or both?
 // http 서버 넣어주자.
+// 이 파일에도 SQL은 들어가야한다. 근거는.... 데이터 저장에 있다. 그러나 상세할 필요는 존재하지 않는다.
+// 그렇게 된다면 쿠버네티스가 필요해진다.
