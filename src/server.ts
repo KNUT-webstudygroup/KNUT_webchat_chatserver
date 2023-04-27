e70636f5f38c1a89843e64a85d96ff728f968133
LuticaCANARD <presan100@gmail.com>
2023-04-27 15:00:27 +0900

    iz

--- src/server.ts
+++ src/server.ts
@@ -7,4 +7,5 @@ import { WebSocket } from "ws";
 // redis로 멤버의 정보를 가져오던지 하자.
 // 여기에서는 채팅과 서버 자신에게 필요한 데이터를 저장시키는 방향으로 가도록 하자.
 // "실행은 도커로 한다. 관리가 편해지기 떄문!!!!!"
+// Do : check about discord server. We need Kafka?  or redis or both?
 // http 서버 넣어주자.
