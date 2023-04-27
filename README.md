3754ae0d2916c69cc5a541ab54189dfc659c825b
LuticaCANARD <presan100@gmail.com>
2023-04-27 14:53:37 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# KNUT WEBCHAT BACKEND
+
+## How to start
+1. Install dependency
+```bash
+npm install
+```
+2. Run!
+```bash
+npm run dev
+```
+
---
+++ src/server.ts
@@ -0,0 +1,10 @@
+// 사실상 깡통
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+import { WebSocket } from "ws";
+
+// redis로 멤버의 정보를 가져오던지 하자.
+// 여기에서는 채팅과 서버 자신에게 필요한 데이터를 저장시키는 방향으로 가도록 하자.
+// "실행은 도커로 한다. 관리가 편해지기 떄문!!!!!"
+// http 서버 넣어주자.
